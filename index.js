83e121c7721f894a3d497f79652a6b3ddb67b149
unknown <Edison Raul Barreiro Zambrano>
2023-07-31 10:40:14 -0500

    postgres agregado

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
-import express from 'express'
+import express, { json } from 'express'
+import pg from "pg"
+import {config} from "dotenv"
+
+const pool = new pg.Pool({connectionString: process.env.DATABASE_URL});
 
 const app = express()
 
-app.get('/', (req, res) => {
-    res.send('hello world')
-})
+// Definir un puerto para nuestro servidor
+const port = 3000 || process.env.PORT;
 
-app.listen(3000)
-console.log('sirviendo en puerto 3000')
+// Definir una ruta de prueba
+app.get('/', (req, res) => {
+  res.send('¡Hola Mundo!');
+});
+
+app.get('/ping', async (req, res) => {
+    const result = await pool.query('SELECT NOW()')
+    return res.json(result.rows[0])
+  });
+
+// Iniciar el servidor
+app.listen(port, () => {
+  console.log(`Servidor escuchando en http://localhost:${port}`);
+});
